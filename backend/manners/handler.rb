2ee26231c1ae4e4c444ae4c3dee2bd154f039ccc
Kingdon Barrett <kingdon.b@nd.edu>
2020-02-02 10:24:21 -0500

    see if we can get to Rails from within OpenFaaS

--- backend/manners/handler.rb
+++ backend/manners/handler.rb
@@ -1,8 +1,10 @@
+require File.expand_path('../../config/environment', __FILE__)
+
 class Handler
   def run(body, headers)
     status_code = 200 # Optional status code, defaults to 200
     response_headers = {"content-type" => "text/plain"}
-    body = "Hello world from the Ruby template"
+    body = "Welcome to Rails, the embedded string is #{"".blank? ? 'blank' : 'not blank'}."
 
     return body, response_headers, status_code
   end
