4a6c624717913265678c2f44cf7faedb3d038e59
Kingdon Barrett <kingdon.b@nd.edu>
2020-02-08 19:56:57 -0500

    db:migrate (add clients array to table)

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_124912) do
+ActiveRecord::Schema.define(version: 2020_02_09_005613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_124912) do
     t.integer "seats"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "clients", default: [], array: true
   end
 
   add_foreign_key "forks", "tables"
