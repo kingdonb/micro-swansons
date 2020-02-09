2033f3968dacf68fcdefe5e01cab6a9c94ecd93a
Kingdon Barrett <kingdon.b@nd.edu>
2020-02-09 01:38:11 -0500

    rails db:migrate (AddOwnerIpToFork)

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_005613) do
+ActiveRecord::Schema.define(version: 2020_02_09_060740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_02_09_005613) do
     t.bigint "table_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "owner_ip"
     t.index ["table_id"], name: "index_forks_on_table_id"
   end
 
