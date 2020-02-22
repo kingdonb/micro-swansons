cb2baf96f9eea89130535176245c7bc7cf68518b
Kingdon Barrett <yebyen@gmail.com>
2020-02-22 09:37:59 -0500

    allow BetterErrors middleware from docker host

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
-  config.web_console.whitelisted_ips = '172.16.0.0/12'
+  # config.web_console.whitelisted_ips = '172.16.0.0/12'
+  BetterErrors::Middleware.allow_ip! '172.16.0.0/12'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
